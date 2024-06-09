d72546e39394d6b15ded79bb4edc8dcc016584c3
Michael Wellman <michael.wellman@fcbtechnical.com>
2024-06-09 17:18:50 -0500

    commit for adding Theme Icon and removing an issue that occured

---
+++ src/components/Greetings.jsx
@@ -0,0 +1,15 @@
+import { useState } from "preact/hooks";
+
+export default function Greeting({ messages }) {
+    const randomMessages = () => messages[(Math.floor(Math.random() * messages.length))];
+
+    const [greeting, setGreeting] = useState(messages[0]);
+
+    return (
+        <div>
+            <h3>{greeting}! Thank you for visiting!</h3>
+
+            <button onClick={() => setGreeting(randomMessages())}>New Greeting</button>
+        </div>
+    );
+}
